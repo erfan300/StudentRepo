ffa5208aac9a5c9e61f307fa60a166ef8d539428
Erfan Soltani <erfan.soltani@icloud.com>
2024-01-22 19:46:45 +0000

    Editted lab01_Java in GitPod

--- Lab Folders/Lab01/Lab01_Java.java
+++ Lab Folders/Lab01/Lab01_Java.java
@@ -5,7 +5,7 @@ void testMethod1()
   //First command x = y + z
   //Second commnad print(x)
   //edit made by ErfanSoltani_220117609
-
+  //edit made by ErfanSoltani_220117609 in GitPod
   //Last line print('Good bye')
 }
 
